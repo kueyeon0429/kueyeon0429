266d994c465ab473d92b35f92963d0d275ead39b
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2023-11-06 23:22:43 +0900

    Create main.py for Update Tistory

---
+++ main.py
@@ -0,0 +1,21 @@
+import feedparser, time
+
+URL="https://noooey.tistory.com/rss"
+RSS_FEED = feedparser.parse(URL)
+MAX_POST=3
+
+markdown_text = """
+### ✍️ isPowerfulBlog...의 근황
+""" # list of blog posts will be appended here
+
+
+for idx, feed in enumerate(RSS_FEED['entries']):
+    if idx > MAX_POST:
+        break
+    else:
+        feed_date = feed['published_parsed']
+        markdown_text += f"[{time.strftime('%Y.%m.%d', feed_date)} | {feed['title']}]({feed['link']}) <br/>\n"
+
+f = open("README.md",mode="w", encoding="utf-8")
+f.write(markdown_text)
+f.close()
