b8bcc8666ffa9e10dda50048fa252ce3b910b93d
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2023-11-08 03:55:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import feedparser, time
 
 URL="https://noooey.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST=3
+MAX_POST=5
 
 markdown_text = """
 
@@ -15,14 +15,14 @@ category = {'AI': 0, 'DataEngineering':0, 'Infra':0, 'Algorithm':0}
 
 idx = 0
 for feed in RSS_FEED['entries']:
-    if idx > MAX_POST:
+    if idx >= MAX_POST:
         break
     else:
         feed_category = feed['category']
-        if feed_category in category.keys() and category[feed_category] == 0:
+        if (feed_category in category.keys()) and (category[feed_category] < 2):
             feed_date = feed['published_parsed']
             markdown_text += f"**[{feed['title']}]({feed['link']})**, {time.strftime('%Y.%m.%d', feed_date)} <br/> \n"
-            category[feed_category] = 1 # checked!
+            category[feed_category] += 1 # count up!
             idx += 1
 
 markdown_text += "\n--- \n"
