24f1b514774362131662df8cc7672eb964b2d5b7
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2023-11-07 17:15:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ MAX_POST=2
 markdown_text = """
 
 **Blog~...✍️**  
-|date   |title                         |
-|-------|------------------------------|
 """ # list of blog posts will be appended here
 
 
@@ -17,7 +15,7 @@ for idx, feed in enumerate(RSS_FEED['entries']):
         break
     else:
         feed_date = feed['published_parsed']
-        markdown_text += f"| {time.strftime('%Y.%m.%d', feed_date)} | [{feed['title']}]({feed['link']}) | \n"
+        markdown_text += f"[{time.strftime('%Y.%m.%d', feed_date)} | {feed['title']}]({feed['link']}) <br/> \n"
 
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
