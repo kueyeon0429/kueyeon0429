e28217f6d99d58211667483df16b6d3be83ad6c8
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2023-11-08 03:47:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -11,15 +11,18 @@ Blog____✍️
 
 """ # list of blog posts will be appended here
 
+category = {'AI': 0, 'DataEngineering':0, 'Infra':0, 'Algorithm':0}
+
 idx = 0
 for feed in RSS_FEED['entries']:
     if idx > MAX_POST:
         break
     else:
         feed_category = feed['category']
-        if feed_category in ['AI', 'DataEngineering', 'Infra']:
+        if feed_category in category.keys() and category[feed_category] == 0:
             feed_date = feed['published_parsed']
             markdown_text += f"**[{feed['title']}]({feed['link']})**, {time.strftime('%Y.%m.%d', feed_date)} <br/> \n"
+            category[feed_category] = 1 # checked!
             idx += 1
 
 markdown_text += "\n--- \n"
