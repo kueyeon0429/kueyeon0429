6fcef8b3046270524a5eaf23f736e3d09d545486
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2023-11-08 03:18:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,20 +2,23 @@ import feedparser, time
 
 URL="https://noooey.tistory.com/rss"
 RSS_FEED = feedparser.parse(URL)
-MAX_POST=2
+MAX_POST=3
 
 markdown_text = """
 
 **Blog~...✍️**  
 """ # list of blog posts will be appended here
 
-
-for idx, feed in enumerate(RSS_FEED['entries']):
+idx = 0
+for feed in enumerate(RSS_FEED['entries']):
     if idx > MAX_POST:
         break
     else:
-        feed_date = feed['published_parsed']
-        markdown_text += f"[{time.strftime('%Y.%m.%d', feed_date)} | {feed['title']}]({feed['link']}) <br/> \n"
+        feed_category = feed['category']
+        if feed_category in ['AI', 'DataEngineering', 'Infra']:
+            feed_date = feed['published_parsed']
+            markdown_text += f"{time.strftime('%Y.%m.%d', feed_date)} | #{feed_category}) <br/> \n ### [{feed['title']}]({feed['link']}) <br/> \n"
+            idx += 1
 
 f = open("README.md",mode="w", encoding="utf-8")
 f.write(markdown_text)
