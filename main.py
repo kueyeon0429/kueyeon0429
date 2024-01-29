37c969a791b66e1ba6b879bf48f059acda460c65
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2024-01-29 21:18:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Blog____✍️
 
 """
 
-category = {'AI': 0, 'DataEngineering':0, 'Infra':0, 'Algorithm':0, 'CS':0, '후기':0}
+category = {'AI': 0, 'DataEngineering':0, 'Infra':0, 'Algorithm':0, 'CS':0}
 
 idx = 0
 for feed in RSS_FEED['entries']:
