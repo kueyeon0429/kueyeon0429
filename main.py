f468b9965c8abb7c1a5a1fd2787b4fcdd27b0d71
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2024-01-29 21:23:30 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ for feed in RSS_FEED['entries']:
         break
     else:
         feed_category = feed['category']
-        if (feed_category in category.keys()) and (category[feed_category] < 1):
+        if (feed_category in category.keys()) and (category[feed_category] < 2):
             feed_date = feed['published_parsed']
             markdown_text += f"**[{feed['title']}]({feed['link']})**, {time.strftime('%Y.%m.%d', feed_date)} <br/> \n"
             category[feed_category] += 1 # count up!
