fd04c72a6b7222734089fa833277dcfe66e6e5df
KyuYeon Park <66217855+noooey@users.noreply.github.com>
2024-06-07 15:19:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ MAX_POST=6
 
 markdown_text = """
 <a href="https://github.com/devxb/gitanimals">
-  <img src="https://render.gitanimals.org/lines/{noooey}?pet-id=1" width="1000" height="120"/>
+  <img src="https://render.gitanimals.org/lines/{noooey}?pet-id=582980161288278816" width="1000" height="120"/>
 </a>
+  
 
 Blog____✍️  
 ---
